72d441dcc69678922f19828e4f018a9ce5e7c9fd
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 21:40:22 -0300

    Add files via upload

--- batalha.h
+++ batalha.h
@@ -1,1 +1,12 @@
+#ifndef BATALHA_H
+#define BATALHA_H
 
+#define TAM 5
+#define NAVIOS 3
+
+void inicializarTabuleiro(char tabuleiro[TAM][TAM]);
+void mostrarTabuleiro(char tabuleiro[TAM][TAM], int mostrarNavios);
+void posicionarNavios(char tabuleiro[TAM][TAM]);
+int atirar(char tabuleiro[TAM][TAM], int linha, int coluna);
+
+#endif
